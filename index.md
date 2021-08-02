631528a7bb0f39c31d9f758d92fce82371360566
rahulan-c <7338147+rahulan-c@users.noreply.github.com>
2021-08-02 14:57:28 +0100

    2021-08-02 14:57:28 - Update

--- index.md
+++ index.md
@@ -1,14 +1,8 @@
-Statistical reports for the [Lichess4545](https://www.lichess4545.com/)
-[Team](https://www.lichess4545.com/team4545/) (45+45) and
-[LoneWolf](https://www.lichess4545.com/lonewolf/) (30+30) online chess
-leagues.
-
 ## All time records
 
 Coming soon.
 
 ## Season statistics and awards
 
-| Lichess4545 League | Lichess LoneWolf (Open)                                                                  | Lichess LoneWolf (U1800) |
-|:-------------------|:-----------------------------------------------------------------------------------------|:-------------------------|
-|                    | [Season 11](https://rahulan-c.github.io/lichess4545-stats/reports/stats_lwopen_s11.html) |                          |
+| Lichess4545 League | Lichess LoneWolf (Open) | Lichess LoneWolf (U1800) |
+|:-------------------|:------------------------|:-------------------------|
--- scripts/make_openings_sunburst.py
+++ scripts/make_openings_sunburst.py
@@ -1,33 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-MAKE OPENINGS SUNBURST PLOT
+Make openings sunburst plots for Lichess4545 seasons
+Source: https://github.com/Destaq/chess_graph
 """
-
-# !pip install --upgrade plotly
-# !pip install --upgrade pgnparser
-# !pip install --upgrade chess_graph
-
-# import plotly.io as pio
-
-# pio.renderers.default='browser'
 
 import chess_graph
 
-# Define path to season games PGN
 pgn_path = "C:/Users/rahul/Documents/Github/rahulan-c.github.io/lichess4545-stats/data/games.pgn"
 
-# Produce openings sunburst plot
+# Make HTML sunburst
 chess_graph.graph(pgn_path, depth=16, shade = False, 
                   fragmentation_percentage=0.004, 
                   should_defragment = True, custom_branching=False, 
                   should_download = True, download_format = 'html', 
                   download_name = 'sunburst')
 
-# Re-do just to save snapshot PNG (for stats report)                  
+# Make PNG sunburst - to show in final report, linked to the (separate) HTML version                  
 chess_graph.graph(pgn_path, depth=16, shade = False, 
                   fragmentation_percentage=0.004, 
                   should_defragment = True, custom_branching=False, 
                   should_download = True, download_format = 'png', 
                   download_name = 'sunburst')
-
-# print("Produced openings sunburst plot!")
